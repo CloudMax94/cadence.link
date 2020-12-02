c5224a487b5f77ad7ad100472ae99da530ad034e
Max Persson <admin@cloudmodding.com>
2020-12-02 19:22:16 +0100

    Subtract scrollbar width from the menus right padding

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 (function () {
+  function getScrollbarWidth() {
+    // Creating invisible container
+    const outer = document.createElement('div')
+    outer.style.visibility = 'hidden'
+    outer.style.overflow = 'scroll' // forcing scrollbar to appear
+    outer.style.msOverflowStyle = 'scrollbar' // needed for WinJS apps
+    document.body.appendChild(outer)
+
+    // Creating inner element and placing it in the container
+    const inner = document.createElement('div')
+    outer.appendChild(inner)
+
+    // Calculating difference between container's full width and the child width
+    const scrollbarWidth = (outer.offsetWidth - inner.offsetWidth)
+
+    // Removing temporary elements from the DOM
+    outer.parentNode.removeChild(outer)
+
+    return scrollbarWidth
+  }
+
   function initializePOI (poi) {
     var startPos
     var clickPos
@@ -24,6 +45,10 @@
   }
 
   document.addEventListener('DOMContentLoaded', function () {
+    // Calculate & set --scrollbar-width for style purposes
+    document.body.style.setProperty('--scrollbar-width', getScrollbarWidth() + 'px')
+
+    // Find and initialize all POIs
     var pois = document.querySelectorAll('.poi')
     for (var poi of pois) {
       initializePOI(poi)
