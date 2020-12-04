ef312a870543de3ba8baeabbfc22b12507719653
Max Persson <admin@cloudmodding.com>
2020-12-04 19:42:09 +0100

    Moved scrollbar width calculation to top of body to prevent jumping

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,4 @@
 (function () {
-  var scrollbarWidth = null
-  function getScrollbarWidth (recalculate = false) {
-    if ((scrollbarWidth != null) && !recalculate) {
-      return scrollbarWidth
-    }
-    var outer = document.createElement('div')
-    outer.style.visibility = 'hidden'
-    outer.style.width = '100px'
-    document.body.appendChild(outer)
-    var widthNoScroll = outer.offsetWidth
-    // force scrollbars
-    outer.style.overflow = 'scroll'
-    // add inner div
-    var inner = document.createElement('div')
-    inner.style.width = '100%'
-    outer.appendChild(inner)
-    var widthWithScroll = inner.offsetWidth
-    // remove divs
-    outer.parentNode.removeChild(outer)
-    scrollbarWidth = widthNoScroll - widthWithScroll
-    return scrollbarWidth
-  }
-
   function initializePOI (poi) {
     var startPos
     var clickPos
@@ -47,9 +24,6 @@
   }
 
   document.addEventListener('DOMContentLoaded', function () {
-    // Calculate & set --scrollbar-width for style purposes
-    document.body.style.setProperty('--scrollbar-width', getScrollbarWidth() + 'px')
-
     // Find and initialize all POIs
     var pois = document.querySelectorAll('.poi')
     for (var poi of pois) {
